14ea6beaf39323540613051d0c4a31942a248a82
Jack Anderson <phrygian.jack@gmail.com>
2016-06-14 17:12:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trailwhisper::Application.config.secret_key_base = '24ae1b238b122f7cec6995311b6589cc422e8c989d2fb2db2d2b47bf2efb4443681eec4cab463c6089c46fc6bfddf9a85146da2b5f3cf7498f48f1444506cecd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trailwhisper::Application.config.session_store :cookie_store, key: '_trailwhisper_session'
