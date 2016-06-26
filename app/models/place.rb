db6d28d9b92ea46c1e0183082e3a04cd6c1a7945
Jack Anderson <phrygian.jack@gmail.com>
2016-06-26 22:52:54 +0000

    Added validation to place creation and update fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: { message: "Field Cannot Be Blank" }, length: {minimum: 3, too_short: "Must Be at Least %{count} Characters."}
+  validates :address, presence: { message: "Field Cannot Be Blank" }
+  validates :description, presence: { message: "Field Cannot Be Blank" }
 end
