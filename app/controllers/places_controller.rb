5d15882f0406c4a5a3a4e2ed2db9e704bee339c1
Jack Anderson <phrygian.jack@gmail.com>
2016-06-26 22:09:35 +0000

    Protected destroy feature

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
@@ -38,6 +38,9 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
