c64febd6b57f2e970166b75489d15f4e83d4b406
Jack Anderson <phrygian.jack@gmail.com>
2016-06-28 14:40:16 +0000

    Added rough comment form

--- db/migrate/20160628140235_create_comments.rb
+++ db/migrate/20160628140235_create_comments.rb
@@ -7,9 +7,9 @@ class CreateComments < ActiveRecord::Migration
       t.integer :user_id
       t.integer :place_id
       t.timestamps
-    end
+     end
 
-    add_index :comments [:user_id, :place_id]
-    add_index :commens, :place_id
+    add_index :comments, [:user_id, :place_id]
+    add_index :comments, :place_id
   end
 end
