c9314fe77abb719f201951877ad5e77aa5eecb3d
Jack Anderson <phrygian.jack@gmail.com>
2016-06-28 14:14:35 +0000

    Added comments model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
  
   validates :name, presence: { message: "Field Cannot Be Blank" }, length: {minimum: 3, too_short: "Must Be at Least %{count} Characters."}
   validates :address, presence: { message: "Field Cannot Be Blank" }
