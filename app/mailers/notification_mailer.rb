01cd2b9207a99e89ce105a14c6cc202777ee8187
Jack Anderson <phrygian.jack@gmail.com>
2016-07-10 20:33:15 +0000

    Send email to place owner

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@trailwhisper.herokuapp.com"
 
-  def comment_added
-    mail(to: "phrygian.jack@gmail.com", 
-              subject: "A comment has been posted to your Trail!")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, 
+         subject: "A comment has been posted to your Trail, #{@place.name}!")
   end
 end
